9912465071ac76ac04489c4457fc1ae41a330176
DeepEnergy <filiprebizant@gmail.com>
2017-02-27 19:54:51 +0100

    finished for desktop

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
-    $('.burger').click(function(){
-       $(this).toggleClass('active'); 
+    $('.burger').click(function () {
+        $(this).toggleClass('active');
+        $('nav').toggleClass('opened');
     });
+
+$(window).scroll(function(){
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > 50) {
+        $('header').addClass('sticky');
+    }
+    else{
+        $('header').removeClass('sticky');
+    }  
+});
+        
+    
+
+
 });

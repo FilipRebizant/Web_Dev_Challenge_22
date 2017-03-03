add1a8091f57e21608925d1a5058099b00371da5
DeepEnergy <filiprebizant@gmail.com>
2017-03-03 13:58:08 +0100

    done some RWD

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,29 @@
 $(document).ready(function () {
     $('.burger').click(function () {
-        $(this).toggleClass('active');
-        $('nav').toggleClass('opened');
+            $(this).toggleClass('active');
+            $('nav').toggleClass('opened');
+
+        if($('nav').hasClass('opened')){
+            $('header').toggleClass('max-height');
+        }
+        else{ 
+            setTimeout(function () {
+                $('header').toggleClass('max-height');
+            }, 300);
+        }
+
     });
 
-$(window).scroll(function(){
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > 50) {
-        $('header').addClass('sticky');
-    }
-    else{
-        $('header').removeClass('sticky');
-    }  
-});
-        
-    
+    $(window).scroll(function () {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > 50) {
+            $('header').addClass('sticky');
+        } else {
+            $('header').removeClass('sticky');
+        }
+    });
+
+
 
 
 });
